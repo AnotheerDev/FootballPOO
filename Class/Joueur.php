2cfae710e8702900b6077b413875a24bd4462b55
AnotheerDev <leger.julien94@gmail.com>
2023-06-16 11:04:43 +0200

    modification class Joueur

--- Class/Joueur.php
+++ Class/Joueur.php
@@ -7,14 +7,16 @@ class Joueur
     private string $nom;
     private string $prenom;
     private DateTime $dateNaissance;
+    private Pays $pays;
 
 
-    public function __construct(int $idJoueur, string $nom, string $prenom, string $dateNaissance)
+    public function __construct(int $idJoueur, string $nom, string $prenom, string $dateNaissance, Pays $pays)
     {
         $this->idJoueur = $idJoueur;
         $this->nom = $nom;
         $this->prenom = $prenom;
         $this->dateNaissance = new DateTime($dateNaissance);
+        $this->pays = $pays;
     }
 
 
@@ -63,5 +65,17 @@ class Joueur
     public function setDateNaissance($dateNaissance)
     {
         $this->dateNaissance = $dateNaissance;
+    }
+
+
+    public function getPays()
+    {
+        return $this->pays;
+    }
+
+
+    public function setPays($pays)
+    {
+        $this->pays = $pays;
     }
 }
