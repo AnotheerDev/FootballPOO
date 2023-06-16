15fefb20a01ea938e3f99b161a7b09b6ec22bf32
AnotheerDev <leger.julien94@gmail.com>
2023-06-16 11:21:01 +0200

    création class Contrat

---
+++ Class/Contrat.php
@@ -0,0 +1,68 @@
+<?php
+
+class Contrat
+{
+    //Attributs:
+    private ClubFoot $clubFoot;
+    private Joueur $joueur;
+    private int $idContrat;
+    private DateTime $dateContrat;
+
+
+    public function __construct(ClubFoot $clubFoot, Joueur $joueur, int $idContrat, string $dateContrat)
+    {
+        $this->clubFoot = $clubFoot;
+        $this->joueur = $joueur;
+        $this->idContrat = $idContrat;
+        $this->dateContrat = new DateTime($dateContrat);
+    }
+
+
+    // getter et setter pour chaque attribut de ma class :
+    public function getclubFoot()
+    {
+        return $this->clubFoot;
+    }
+
+
+    public function setclubFoot($clubFoot)
+    {
+        $this->clubFoot = $clubFoot;
+    }
+
+
+    public function getjoueur()
+    {
+        return $this->joueur;
+    }
+
+
+    public function setjoueur($joueur)
+    {
+        $this->joueur = $joueur;
+    }
+
+
+    public function getIdContrat()
+    {
+        return $this->idContrat;
+    }
+
+
+    public function setIdContrat($idContrat)
+    {
+        $this->idContrat = $idContrat;
+    }
+
+
+    public function getDateContrat()
+    {
+        return $this->dateContrat;
+    }
+
+
+    public function setDateContrat($dateContrat)
+    {
+        $this->dateContrat = $dateContrat;
+    }
+}
