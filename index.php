df54fc3cf99994f0acd63dbf5649617b06bbe168
AnotheerDev <leger.julien94@gmail.com>
2023-06-16 09:54:07 +0200

    Création index

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+// façon plus rapide de faire ( autochargement des class) ça evite d'oublier des class sur des plus gros projets :
+spl_autoload_register(function ($class_name) {
+    require 'class/' . $class_name . '.php';
+});
